aca10c76275218bdc91b9ef2fcc8ab241cf5f3b1
SunFengXin666 <145885675+SunFengXin666@users.noreply.github.com>
2026-05-09 12:31:35 +0800

    Add register UI and beautify login

--- app/src/main/java/com/imcloud/app/navigation/Screen.kt
+++ app/src/main/java/com/imcloud/app/navigation/Screen.kt
@@ -3,5 +3,6 @@ package com.imcloud.app.navigation
 sealed class Screen(val route: String) {
     object Splash : Screen("splash")
     object Login : Screen("login")
+    object Register : Screen("register")
     object Chat : Screen("chat")
 }
